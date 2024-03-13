dab75954bddb09c75691510c43d164080b847056
eliza-pimentel <elizapimentel@hotmail.com>
2024-03-13 16:53:03 -0300

    add router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {ProductRoutes} from "./products/product-routing.module";
+
+const routes: Routes = [
+  { path: '',
+    redirectTo: '/products',
+    pathMatch: 'full' },
+
+  ...ProductRoutes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
