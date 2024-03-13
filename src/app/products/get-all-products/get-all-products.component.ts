78c6081ad4681cb8076c0b8b780b9034449fa40b
eliza-pimentel <elizapimentel@hotmail.com>
2024-03-13 17:02:08 -0300

    change how to call the method

--- src/app/products/get-all-products/get-all-products.component.ts
+++ src/app/products/get-all-products/get-all-products.component.ts
@@ -1,28 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../../service/product.service';
-import { Product } from '../../model/product.model';
+import { ProductService } from '../service/product.service';
+import { Product } from '../model/product.model';
 
 @Component({
   selector: 'app-get-all-products',
   templateUrl: './get-all-products.component.html',
   styleUrl: './get-all-products.component.css'
 })
-export class GetAllProductsComponent implements OnInit{
+export class GetAllProductsComponent implements OnInit {
 
   showProducts: Product[] = [];
 
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.productService.getAll().subscribe( (products: Product[] ) =>
-    this.showProducts = products)
- }
+    this.getAllProducts();
+  }
 
-
-
-
-
+  getAllProducts(): void {
+    this.productService.getAll().subscribe((products: Product[]) => {
+      this.showProducts = products;
+    });
+  }
 }
-
-
-// ver topico da udemy 51 e 52 (criar componentes e registrar rotas)
