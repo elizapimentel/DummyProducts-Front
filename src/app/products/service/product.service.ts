88188f630fac934f5bd481c7e7c3cb47eb8f7d64
eliza-pimentel <elizapimentel@hotmail.com>
2024-03-13 16:50:47 -0300

    add cache service and change de URL to consume DB

--- src/app/products/service/product.service.ts
+++ src/app/products/service/product.service.ts
@@ -2,24 +2,34 @@ import { Injectable } from '@angular/core';
 import { Product } from '../model/product.model';
 import { HttpClient } from '@angular/common/http';
 import {map, Observable} from 'rxjs';
+import {CacheService} from "./cache.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private URL = 'http://localhost:8080/api/products';
+  URL = 'http://localhost:8080/api/products/db';
 
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient, private cacheService: CacheService ) { }
 
   getAll(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.URL).pipe(
-      map((products: Product[]) => {
-        products.forEach(product => {
-          product.price = parseFloat(product.price).toFixed(2);
-        });
-        return products;
-      })
-    );
+    const cachedData = this.cacheService.get('products');
+    if (cachedData) {
+      return new Observable<Product[]>(observer => {
+        observer.next(cachedData);
+        observer.complete();
+      });
+    } else {
+      return this.http.get<Product[]>(this.URL).pipe(
+        map((products: Product[]) => {
+          products.forEach(product => {
+            product.price = parseFloat(product.price).toFixed(2);
+          });
+          this.cacheService.set('products', products); // Salve os dados em cache
+          return products;
+        })
+      );
+    }
   }
 }
