46d9c14cf7dd5206e470856c0bc23f762d8545ff
eliza-pimentel <elizapimentel@hotmail.com>
2024-03-20 12:43:35 -0300

    add core

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from "@angular/router";
 
 import { ProductService } from './service/product.service';
+import { CreateNewProductComponent } from './create/create-new-product.component';
+import {FormsModule} from "@angular/forms";
+import {GetAllProductsComponent} from "./getAll/get-all-products.component";
+import { CoreModule } from '../core/core.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GetAllProductsComponent,
+    CreateNewProductComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    CoreModule
   ],
   providers: [ProductService]
 })
