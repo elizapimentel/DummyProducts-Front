6d7c56927b9ee8ec9b2cf45c5e1729d72b1e9d3a
eliza-pimentel <elizapimentel@hotmail.com>
2024-03-20 12:42:56 -0300

    change images format

--- src/app/products/model/product.model.ts
+++ src/app/products/model/product.model.ts
@@ -1,27 +1,14 @@
 export interface Product {
 
-      id: number;
+      id?: number;
       title: string,
       description: string;
+      price: any;
       discountPercentage: number;
       rating: number;
       stock: number;
       brand: string;
       category: string;
       thumbnail: string;
-      price: any;
-      images: Image[];
+      images: string[];
 }
-export interface Image {
-    description: string;
-    url: string;
-}
-
-
-/*
-Se com a interface n funcionar, tentar com a classe
-let arr: number[] = [];
-let arr: Array<number> = [];
-
-https://oieduardorabelo.medium.com/typescript-entendendo-a-nota%C3%A7%C3%A3o-de-tipos-9e8c1c89ef62
-*/
