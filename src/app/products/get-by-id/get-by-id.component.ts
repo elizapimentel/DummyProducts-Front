85b4e6bf72781c046416cac1cad0f05f8b8dc90b
eliza-pimentel <elizapimentel@hotmail.com>
2024-03-20 17:01:36 -0300

    add get product by id

---
+++ src/app/products/get-by-id/get-by-id.component.ts
@@ -0,0 +1,30 @@
+import { Component, ViewChild } from '@angular/core';
+import { ProductService } from '../service/product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../model/product.model';
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-get-by-id',
+  templateUrl: './get-by-id.component.html',
+  styleUrls: ['./get-by-id.component.css']
+})
+export class GetByIdComponent {
+
+  @ViewChild('formProduct', { static: true }) formProduct: NgForm;
+  products: Product;
+
+  constructor(private service: ProductService, private router: Router,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.findProductById();
+  }
+
+  findProductById() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.service.getById(id).subscribe(item => this.products = item);
+  }
+
+}
